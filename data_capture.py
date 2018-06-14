87ebfb827813fba8e86e831c81ff711ee8e2f297
Owner <owner@Owners-MacBook-Pro.local>
2018-06-14 09:11:28 +0800

    updates

--- data_capture.py
+++ data_capture.py
@@ -11,7 +11,7 @@ def on_press(val, img):
     global img_count
     with open('actions.txt', 'a') as f:
         f.write('{}\n'.format(val))
-    mss.tools.to_png(img.rgb, img.size, output=r'./images/{0}_{1}.png'.format(val, img_count))
+    mss.tools.to_png(img.rgb, img.size, output=r'./images/frame-{0}_action-{1}.png'.format(img_count, val))
     img_count += 1
 
 def main():
@@ -40,22 +40,22 @@ def main():
 
 if __name__ == '__main__':
     mon = {
-            'top': 0,
+            'top': 60,
             'left': 0,
-            'width': 0,
-            'height': 0
+            'width': 420,
+            'height': 720
             }
     
     sct = mss.mss()
     img_count = 0
 
-    if os.path.exists(r'./images'):
-        overwrite = (input('Image folder exists. Do you want to overwrite? Y/N: ')).lower()
-        if overwrite == 'y':
-            pass
-        elif overwrite == 'n':
-            sys.exit()
-    else:
-        os.mkdir(r'./images')
+    #if os.path.exists(r'./images'):
+    #    overwrite = (input('Image folder exists. Do you want to overwrite? Y/N: ')).lower()
+    #    if overwrite == 'y':
+    #        pass
+    #    elif overwrite == 'n':
+    #        sys.exit()
+    #else:
+    #    os.mkdir(r'./images')
 
     main()
